7d0b1e676cd7780e428dff38ffb75084fab810c9
Harvy-silzel <harvysilgut@gmail.com>
2024-06-15 13:48:34 -0600

    packend.json actualizado

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'hola mundo aqui no hay nada que ver!';
+    return 'hola mundo aqui no hay nada que ver valla a la pagina principal!';
   }
 }
