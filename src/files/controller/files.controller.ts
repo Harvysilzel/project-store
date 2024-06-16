ccc50e20cd033c6a02335e5134d26f01020c5d4d
Harvy-silzel <harvysilgut@gmail.com>
2024-06-16 00:52:52 -0600

    preparar back 4

--- src/files/controller/files.controller.ts
+++ src/files/controller/files.controller.ts
@@ -1,64 +1,58 @@
 import { BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from '../services/files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from 'src/helpers/fileFilter.helper';
+import { fileFilter } from '../../helpers/fileFilter.helper'; // Ajusta la ruta de importación según tu estructura de directorios
 import { diskStorage } from 'multer';
-import { fileNamer } from 'src/helpers/fileNamer.helper';
+import { fileNamer } from '../../helpers/fileNamer.helper'; // Ajusta la ruta de importación según tu estructura de directorios
 import { Response } from 'express';
 
-
 @Controller('files')
-export class FilesController{
-    constructor(private readonly filesService: FilesService){}
-   
+export class FilesController {
+    constructor(private readonly filesService: FilesService) {}
+
     @Post('upload')
     @UseInterceptors(FileInterceptor('file', {
-        //llamamos al fileFilter de Multer y le asignamos nuestro helper
         fileFilter: fileFilter,
-
         storage: diskStorage({
             destination: './static/products',
             filename: fileNamer,
         }),
+    }))
+    uploadFile(@UploadedFile() file: Express.Multer.File) {
+        if (!file) {
+            throw new BadRequestException('Asegurese que el archivo es una imagen');
+        }
 
-    }),
-    )
+        const url = `${file.filename}`;
+        return { url };
+    }
 
     @Post('uploadusers')
     @UseInterceptors(FileInterceptor('file', {
         fileFilter: fileFilter,
         storage: diskStorage({
             destination: './static/users',
-            filename:fileNamer,
+            filename: fileNamer,
         }),
-    } ),
-    )
-
-    uploadFile(@UploadedFile() file: Express.Multer.File) {
-        if(!file){
+    }))
+    uploadUserFile(@UploadedFile() file: Express.Multer.File) {
+        if (!file) {
             throw new BadRequestException('Asegurese que el archivo es una imagen');
         }
 
         const url = `${file.filename}`;
-
         return { url };
     }
 
     @Get('product/:imageName')
-    findProduct(@Res() res:Response, @Param('imageName') imageName: string){
+    findProduct(@Res() res: Response, @Param('imageName') imageName: string) {
         const path = this.filesService.getStaticImageName(imageName);
-
-        //return path;
         res.sendFile(path);
     }
-    
+
     @Get('user/:imageName')
-    findUser(@Res() res:Response, @Param('imageName') imageName: string){
+    findUser(@Res() res: Response, @Param('imageName') imageName: string) {
         const path = this.filesService.getStaticImageName(imageName);
-
-        //return path;
         res.sendFile(path);
     }
-
-
-}   
+}
