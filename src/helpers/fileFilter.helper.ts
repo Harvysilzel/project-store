6661905f7382cef79714c2c16199baa7fcbc0a45
Harvy-silzel <harvysilgut@gmail.com>
2024-06-16 00:35:58 -0600

    preparar back 3

--- src/helpers/fileFilter.helper.ts
+++ src/helpers/fileFilter.helper.ts
@@ -1,15 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fileFilter = void 0;
-const fileFilter = (req, file, callback) => {
-    if (!file)
-        return callback(new Error('Archivo vacío'), false);
-    const fileExtension = file.mimetype.split('/')[1];
-    const validExtension = ['jpg', 'png', 'jpeg', 'JPG', 'PNG'];
-    if (validExtension.includes(fileExtension)) {
-        return callback(null, true);
-    }
-    callback(null, false);
-};
-exports.fileFilter = fileFilter;
-//# sourceMappingURL=fileFilter.helper.js.map
+export declare const fileFilter: (req: Express.Request, file: Express.Multer.File, callback: any) => any; 
